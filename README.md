58c237fc2fc6c3368024f21ebcff6d1abb25b3de
Nikita Karpuk <bitxon.dev@gmail.com>
2024-02-25 19:20:43 +0100

    Add JsonUnwrapped example

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 - [Java Enum](src/test/java/bitxon/jackson/JavaEnumTest.java)
 - [Java Record](src/test/java/bitxon/jackson/JavaRecordTest.java)
 - [JsonView](src/test/java/bitxon/jackson/JsonViewTest.java)
+- [JsonUnwrapped](src/test/java/bitxon/jackson/JsonUnwrappedTest.java)
 - [Polymorphic Deserialization - Name-based](src/test/java/bitxon/jackson/PolymorphismByNameTest.java)
 - [Polymorphic Deserialization - Deduction-based](src/test/java/bitxon/jackson/PolymorphismByDeductionTest.java)
 
---
+++ src/test/java/bitxon/jackson/JsonUnwrappedTest.java
@@ -0,0 +1,56 @@
+package bitxon.jackson;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.Test;
+
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class JsonUnwrappedTest {
+    // ------------------------------ Configuration ---------------------------------------------------
+
+    record Address(String street, String city) {}
+    record User(String fullName, @JsonUnwrapped Address address) {}
+
+    ObjectMapper objectMapper = JsonMapper.builder().build();
+
+
+// ------------------------------ Data ------------------------------------------------------------
+
+    static final String JSON = """
+        {
+          "fullName" : "Mike Walker",
+          "street" : "Main Street",
+          "city" : "New York"
+        }
+        """;
+    static final User USER = new User(
+        "Mike Walker",
+        new Address(
+            "Main Street",
+            "New York"
+        ));
+
+// ------------------------------ Tests -----------------------------------------------------------
+
+
+    @Test
+    void javaToJson() throws Exception {
+        // when
+        var result = objectMapper.writeValueAsString(USER);
+        // then
+        assertThatJson(result).isEqualTo(JSON);
+    }
+
+    @Test
+    void jsonToJava() throws Exception {
+        assertThatThrownBy(() -> objectMapper.readValue(JSON, User.class))
+            .isInstanceOf(Exception.class)
+            .hasMessageContaining("`@JsonUnwrapped`: combination not yet supported");
+    }
+}
